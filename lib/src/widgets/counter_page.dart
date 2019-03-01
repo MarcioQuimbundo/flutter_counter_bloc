47756a2655e1648b661ff65edcefb9af65db51fd
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-03-01 23:37:04 +0100

    counterPage finished

--- lib/src/widgets/counter_page.dart
+++ lib/src/widgets/counter_page.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter_bloc/src/blocs/counter_bloc.dart';
 import 'package:meta/meta.dart';
 
 class CounterPage extends StatelessWidget {
-  //final CounterBloc _counterBloc = CounterBloc;
-
   @override
   Widget build(BuildContext context) {
+    final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Counter Page"),
+      ),
+      body: BlocBuilder<CounterEvent, int>(
+        bloc: _counterBloc,
+        builder: (BuildContext context, int count) {
+          return Center(
+            child: Text("$count", style: TextStyle(fontSize: 24.0),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                _counterBloc.dispatch(CounterEvent.increment);
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () {
+                _counterBloc.dispatch(CounterEvent.decrement);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
